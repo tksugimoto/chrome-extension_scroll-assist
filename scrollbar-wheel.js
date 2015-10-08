3b63339ac22b43bc628cdf052e8da166cf1c6516
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-10-08 13:26:30 +0900

    ページ全体ではない要素のスクロールバーに対応

--- scrollbar-wheel.js
+++ scrollbar-wheel.js
@@ -1,15 +1,25 @@
 window.addEventListener("wheel", function(evt) {
+    var factor = 6;
+    var isVerticalScroll, isHorizontalScroll, scrollTarget;
+    
     if (evt.target.tagName === "HTML") {
-        var isVerticalScroll = evt.clientX > document.documentElement.clientWidth;
-        var isHorizontalScroll = evt.clientY > document.documentElement.clientHeight;
-        
-        var factor = 6;
-        if (isVerticalScroll && !isHorizontalScroll) {
-            document.body.scrollTop += evt.deltaY * factor;
-            evt.returnValue = false;
-        } else if (!isVerticalScroll && isHorizontalScroll) {
-            document.body.scrollLeft += evt.deltaY * factor;
-            evt.returnValue = false;
-        }
+        // ページ全体のスクロールバー
+        isVerticalScroll = evt.clientX > document.documentElement.clientWidth;
+        isHorizontalScroll = evt.clientY > document.documentElement.clientHeight;
+        scrollTarget = document.body;
+    } else {
+        var target = evt.target;
+        // offsetX, offsetY: target左上を(0, 0)とした座標
+        isVerticalScroll = target.scrollHeight > target.clientHeight && evt.offsetX > target.clientWidth;
+        isHorizontalScroll = target.scrollWidth > target.clientWidth && evt.offsetY > target.clientHeight;
+        scrollTarget = target;
+    }
+    
+    if (isVerticalScroll && !isHorizontalScroll) {
+        scrollTarget.scrollTop += evt.deltaY * factor;
+        evt.returnValue = false;
+    } else if (!isVerticalScroll && isHorizontalScroll) {
+        scrollTarget.scrollLeft += evt.deltaY * factor;
+        evt.returnValue = false;
     }
 });

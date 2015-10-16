632fefa577fefd045d8d47c6e14a8d10801a258f
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-10-16 16:26:14 +0900

    コメント追加

--- scrollbar-wheel.js
+++ scrollbar-wheel.js
@@ -4,6 +4,7 @@ window.addEventListener("wheel", function(evt) {
     
     if (evt.target.tagName === "HTML") {
         // ページ全体のスクロールバー
+        // clientX, clientY: ブラウザウィンドウの左上を(0, 0)とした座標
         isVerticalScroll = evt.clientX > document.documentElement.clientWidth;
         // （doctypeなどの影響？）document.documentElement.clientHeightが使えない（bodyのheightを返す）場合に誤作動することを防ぐ
         isHorizontalScroll = evt.clientY > Math.min(document.documentElement.clientHeight, window.innerHeight);
@@ -11,6 +12,7 @@ window.addEventListener("wheel", function(evt) {
     } else if (evt.target.tagName !== "BODY") {
         var target = evt.target;
         // offsetX, offsetY: target左上を(0, 0)とした座標
+        // スクロールバーが出ているか && 領域外か（＝スクロールバー上か）
         isVerticalScroll = target.scrollHeight > target.clientHeight && evt.offsetX > target.clientWidth;
         isHorizontalScroll = target.scrollWidth > target.clientWidth && evt.offsetY > target.clientHeight;
         scrollTarget = target;
